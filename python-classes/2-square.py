dc4f5ea0c4deb95869d2a8342beb32087cf7e58f
k-ganda <k.ganda@alustudent.com>
2023-03-13 08:59:53 -0700

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
         Args:
             size (int): The size of a new square.
         """
-        if not insinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
