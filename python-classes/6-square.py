72f4439a8fd892d39b591acbc6ee0d1670aedc71
k-ganda <k.ganda@alustudent.com>
2023-03-14 07:20:49 -0700

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -48,12 +48,10 @@ class Square:
 
 
     def area(self):
-        """Return the current area of the square."""
         return (self.__size * self.__size)
 
 
     def my_print(self):
-        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
