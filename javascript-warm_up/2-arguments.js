9b40af33d8489620fb2bea48d9e46313a293b474
k-ganda <k.ganda@alustudent.com>
2023-06-07 10:02:38 -0700

    arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args === 3) {
+if (args.length === 3) {
   console.log('Argument found');
-} else if (args > 3) {
+} else if (args.length > 3) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
